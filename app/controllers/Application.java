0c3f271793da86c2c3de1937a045a7cd30b97a14
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-03-26 15:28:22 +0000

    initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import org.owasp.esapi.ESAPI;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.main;
+
+public class Application extends Controller {
+
+    public static Result index(){
+        DynamicForm requestData = Form.form().bindFromRequest();
+        String myname = ESAPI.encoder().encodeForHTML(requestData.get("whatever"));
+        if(myname == null) {
+            return ok(main.render()) ;
+        } else {
+            return ok("<html><body>" + "Hello " + myname + "</body></html>").as("text/html; charset=utf-8");
+        }
+    }
+
+}
